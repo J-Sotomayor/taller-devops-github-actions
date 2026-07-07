4116f3ca6f3a6ec82cb57285ef995f3b42ca5107
Byron Moreno <correo@ejemplo.com>
2026-07-07 10:22:55 -0500

    agregar atributos de cliente

--- src/main/java/com/taller/Cliente.java
+++ src/main/java/com/taller/Cliente.java
@@ -1,4 +1,13 @@
 package com.taller;
 
 public class Cliente {
+    private String nombre;
+    private String cedula;
+    private String correo;
+
+    public Cliente(String nombre, String cedula, String correo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+    }
 }
