776d1057838c2841cb6cb646044308f6b27a375d
victor1997H <vdy.hualpa@yavirac.edu.ec>
2026-07-07 10:15:37 -0500

    feat: agregar atributos de Producto

--- src/main/java/com/taller/Producto.java
+++ src/main/java/com/taller/Producto.java
@@ -1,4 +1,11 @@
 package com.taller;
 
 public class Producto {
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
